fc112a87bd20c50da8e9c2ed55de9eea0e821a9d
Danilo Dos Santos <112526744+Danilonil@users.noreply.github.com>
2023-03-02 19:09:56 -0300

    revisão
    formatação do pc

--- dev/Scripts/activate.bat
+++ dev/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\danil\meus projetos\campeonato\dev
+set VIRTUAL_ENV=D:\danil\Meus Projetos\Python\campeonato\dev
 
 if not defined PROMPT set PROMPT=$P$G
 
